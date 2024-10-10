672c4eb746ee1ce2e020256570c8c9c094ed78e0
Maryam <morrryyyy21@gmail.com>
2024-10-10 21:01:05 +0100

    adds two numbers

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''This is the 0-add module'''
+
+
+def add(a: float, b: float) -> float:
+    """Adds two floats and returns
+        Args:
+        a(float): first number.
+        b(float): secomd number.
+
+        Returns:
+        (float): the sume of the two numbers."""
+    return a + b
