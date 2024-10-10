c973a8b4ef0755a917e4748eed7029d83b582e24
Maryam <morrryyyy21@gmail.com>
2024-10-10 22:20:45 +0100

    return the floor value of a float"

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,7 +1,14 @@
 #!/usr/bin/env python3
-'''2-floor.'''
+'''2-floor: Function to return the largest integer less than or equal to a float.'''
 import math
 
-
 def floor(n: float) -> int:
+    """Returns the floor of a float value.
+
+    Args:
+        n (float): The input float value.
+
+    Returns:
+        int: The largest integer less than or equal to the input float.
+    """
     return math.floor(n)
