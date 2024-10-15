77ceb3a64a808b773c037f894001c05124b01fe3
Maryam <morrryyyy21@gmail.com>
2024-10-15 08:41:25 +0100

    create a basic coroutine that waits

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""0-basic_async_syntax.py"""
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    """
+    A coroutine that waits for a while, and returns the amount of time delayed.
+
+    Args:
+        max_delay(int): the maximum waiting time
+
+    Returns:
+    delay(float): the actual waiting time.
+    """
+    delay = random.uniform(0, max_delay)
+    await asyncio.sleep(delay)
+    return delay
