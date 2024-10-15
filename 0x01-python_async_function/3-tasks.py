6a737134ee44c83d2f7384d59b84c87c253fc5e8
Maryam <morrryyyy21@gmail.com>
2024-10-15 10:43:35 +0100

    return a task

---
+++ 0x01-python_async_function/3-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""3-tasks"""
+import asyncio
+from typing import Any
+wait_random = __import__('1-concurrent_coroutines').wait_random
+
+
+def task_wait_random(max_delay: int) -> asyncio.Task[Any]:
+    """Creates a task from an existing coroutine.
+    Args:
+        max_delay(int): the maximum delay time.
+    Returns:
+    """
+    task = asyncio.create_task(wait_random(max_delay))
+    return task
